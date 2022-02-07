8f10e77d2f64a1134bbfaca84659061ca14568ca
Ikechukwu.Ebele <ebeleikechukwu@gmail.com>
2022-02-07 23:43:46 +0100

    first comment

---
+++ src/main/java/com/example/RestService/RestServiceApplication.java
@@ -0,0 +1,19 @@
+package com.example.RestService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestServiceApplication.class, args);
+
+
+		System.out.println("Server has started @ port 8080. lunch browser to see............................... ");
+
+	}
+
+
+
+}
